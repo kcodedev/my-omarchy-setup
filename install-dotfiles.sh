4de887dde24d181bddc09b29c24c451411fca3e7
kcodedev <40712675+kcodedev@users.noreply.github.com>
2026-01-11 10:38:00 +0700

    Initial commit: Omarchy setup scripts

---
+++ install-dotfiles.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# 
+ORIGINAL_DIR=$(pwd)
+
+REPO_URL="git@github.com:kcodedev/dotfiles.git"
+REPO_NAME="dotfiles"
+
+is_stow_installed() {
+  pacman -Qi "stow" &> /dev/null
+}
+
+if ! is_stow_installed; then
+  echo "Install stow first"
+  exit 1
+fi
+
+cd ~
+
+# Check if the repository already exists
+if [ -d "$REPO_NAME" ]; then
+  echo "Repository '$REPO_NAME' already exists. Skipping clone"
+else
+  git clone "$REPO_URL"
+fi
+
+# Check if the clone was successful
+if [ $? -eq 0 ]; then
+  echo "removing old configs"
+  rm -rf ~/.config/helix
+
+  cd "$REPO_NAME"
+  stow bashrc
+  stow tmux
+  stow helix
+else
+  echo "Failed to clone the repository."
+  exit 1
+fi
+
---
+++ master-installation.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. ./install-stow.sh
+. ./install-helix.sh
+. ./install-keepassxc.sh
+. ./install-taskwarrior.sh
+. ./install-brave-bin.sh
+. ./install-tmux.sh
+. ./install-visual-studio-code-bin.sh
+. ./install-cursor-bin.sh
+. ./install-yazi.sh
+. ./install-podman.sh
+. ./install-hyprland-overrides.sh
+. ./install-dotfiles.sh
+
